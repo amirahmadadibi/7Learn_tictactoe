a011e8c42fa80689fbf00e8817c2d81763e4a202
amirahmadadibi <amirahmadadibi@gmail.com>
2023-05-11 19:32:31 +0330

    S07 E03 add title of each user turn x or o

--- lib/main.dart
+++ lib/main.dart
@@ -11,30 +11,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: GridView.builder(
-              itemCount: 9,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10),
-              itemBuilder: (context, index) {
-                return Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                );
-              })
-          // GridView.count(
-          //   crossAxisCount: 3,
-          //   crossAxisSpacing: 10,
-          //   mainAxisSpacing: 10,
-          //   children: [
-          //     Text('text'),
-          //     Text('text'),
-          //     Text('text'),
-          //     Text('text'),
-          //     Text('text'),
-          //   ],
-          // ),
-          ),
+          body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              'turn O',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Expanded(
+              child: GridView.builder(
+                itemCount: 9,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10),
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
