0be380daedc3231cf3265d85499dc209274a17b3
amirahmadadibi <amirahmadadibi@gmail.com>
2023-07-11 00:32:45 +0330

    redegsin tic tac toe part 2

--- lib/main.dart
+++ lib/main.dart
@@ -14,25 +14,47 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   bool isTurnO = true;
   var moveList = ['', '', '', '', '', '', '', '', ''];
+  var winPositions = [-1, -1, -1];
   int filedPositions = 0;
   String winnerTitle = '';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'pm'),
       home: Scaffold(
           backgroundColor: Colors.white,
           body: SafeArea(
             child: Column(
               children: [
                 SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                    height: 36,
+                    width: 148,
+                    child: Image.asset('assets/images/logo.png')),
+                SizedBox(
                   height: 50,
                 ),
-                Text(
-                  'Turn',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xffAAB0BD)),
+                Visibility(
+                  visible: winnerTitle.isNotEmpty,
+                  child: Text(
+                    'game is over : $winnerTitle',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xffAAB0BD)),
+                  ),
+                ),
+                Visibility(
+                  visible: winnerTitle.isEmpty,
+                  child: Text(
+                    'Turn',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xffAAB0BD)),
+                  ),
                 ),
                 Text(
                   isTurnO ? 'O' : 'X',
@@ -52,7 +74,7 @@ class _MyAppState extends State<MyApp> {
                     },
                     child: Container(
                       height: 39,
-                      width: 131,
+                      width: 150,
                       decoration: BoxDecoration(
                           color: Color(0xff33428DED),
                           borderRadius: BorderRadius.circular(10)),
@@ -77,13 +99,6 @@ class _MyAppState extends State<MyApp> {
                 SizedBox(
                   height: 50,
                 ),
-                Visibility(
-                  visible: winnerTitle.isNotEmpty,
-                  child: Text(
-                    'game is over : $winnerTitle',
-                    style: TextStyle(fontSize: 22),
-                  ),
-                ),
                 SizedBox(
                   height: 50,
                 ),
@@ -105,12 +120,20 @@ class _MyAppState extends State<MyApp> {
                             height: 100,
                             width: 100,
                             decoration: BoxDecoration(
-                                color: Color(0xffF5F5F5),
+                                color: (winnerTitle.isNotEmpty &&
+                                        winPositions.contains(index))
+                                    ? Color(0xff1A007BEC)
+                                    : Color(0xfff5f5f5),
                                 borderRadius:
                                     BorderRadius.all(Radius.circular(10))),
                             child: Center(
                               child: Text('${moveList[index]}',
-                                  style: TextStyle(fontSize: 60)),
+                                  style: TextStyle(
+                                      fontSize: 60,
+                                      color: (winnerTitle.isNotEmpty &&
+                                              winPositions.contains(index))
+                                          ? Color(0xff007BEC)
+                                          : Colors.black)),
                             ),
                           ),
                         );
@@ -130,6 +153,7 @@ class _MyAppState extends State<MyApp> {
       moveList = ['', '', '', '', '', '', '', '', ''];
       filedPositions = 0;
       winnerTitle = '';
+      winPositions = [-1, -1, -1];//0 0 0 
     });
   }
 
@@ -139,7 +163,8 @@ class _MyAppState extends State<MyApp> {
       return;
     }
     filedPositions = filedPositions + 1;
-    print(filedPositions);
+
+    print(winPositions);
     setState(() {
       if (isTurnO) {
         moveList[index] = 'O';
@@ -155,8 +180,6 @@ class _MyAppState extends State<MyApp> {
       }
 
       isTurnO = !isTurnO;
-
-      print(moveList);
     });
   }
 
@@ -164,45 +187,60 @@ class _MyAppState extends State<MyApp> {
     if (moveList[0] == moveList[1] &&
         moveList[0] == moveList[2] &&
         moveList[0].isNotEmpty) {
+      winPositions = [0, 1, 2];
       return 1;
     }
     if (moveList[3] == moveList[4] &&
         moveList[3] == moveList[5] &&
         moveList[3].isNotEmpty) {
+      winPositions = [3, 4, 5];
+
       return 1;
     }
     if (moveList[6] == moveList[7] &&
         moveList[6] == moveList[8] &&
         moveList[6].isNotEmpty) {
+      winPositions = [6, 7, 8];
+
       return 1;
     }
     if (moveList[0] == moveList[3] &&
         moveList[0] == moveList[6] &&
         moveList[0].isNotEmpty) {
+      winPositions = [0, 3, 6];
+
       return 1;
     }
     if (moveList[1] == moveList[4] &&
         moveList[1] == moveList[7] &&
         moveList[1].isNotEmpty) {
+      winPositions = [1, 4, 7];
+
       return 1;
     }
     if (moveList[2] == moveList[5] &&
         moveList[2] == moveList[8] &&
         moveList[2].isNotEmpty) {
+      winPositions = [2, 5, 8];
+
       return 1;
     }
     if (moveList[0] == moveList[4] &&
         moveList[0] == moveList[8] &&
         moveList[0].isNotEmpty) {
+      winPositions = [0, 4, 8];
+
       return 1;
     }
     if (moveList[2] == moveList[4] &&
         moveList[2] == moveList[6] &&
         moveList[2].isNotEmpty) {
+      winPositions = [2, 4, 6];
       return 1;
     }
 
     if (filedPositions == 9) {
+      winPositions = [-1, -1, -1];
       return 0;
     }
 
